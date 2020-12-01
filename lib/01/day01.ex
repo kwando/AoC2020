dd4a304a8de9354ba38d9a895f2438b5b50c4a34
Hannes Nevalainen <hannes.nevalainen@me.com>
2020-12-01 06:45:16 +0100

    day1 part2

--- lib/01/day01.ex
+++ lib/01/day01.ex
@@ -2,27 +2,51 @@ defmodule Aoc2020.Day01 do
   def part1() do
     path = "lib/01/input.txt"
 
-    numbers =
-      path
-      |> File.read!()
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.to_integer/1)
+    numbers = read_numbers(path)
 
     {a, b} = find_sum(numbers, 2020)
 
-    IO.inspect(a: a, b: b, product: a * b)
+    IO.inspect(a: a, b: b, sum: a + b, product: a * b)
+  end
+
+  def part2() do
+    numbers = read_numbers("lib/01/input.txt")
+
+    sum = 2020
+
+    Enum.find(numbers, fn number ->
+      diff = sum - number
+      find_sum(numbers, diff)
+    end)
+    |> case do
+      nil ->
+        nil
+
+      a ->
+        {b, c} = find_sum(numbers, sum - a)
+        IO.inspect(a: a, b: b, c: c, sum: a + b + c, product: a * b * c)
+    end
   end
 
   def find_sum(numbers, sum) do
-    existing_numers = MapSet.new(numbers)
+    existing_numers = numbers
 
-    number =
-      Enum.find(numbers, fn number ->
-        diff = sum - number
+    Enum.find(numbers, fn number ->
+      diff = sum - number
 
-        MapSet.member?(existing_numers, diff)
-      end)
+      MapSet.member?(existing_numers, diff)
+    end)
+    |> case do
+      nil -> nil
+      number -> {number, sum - number}
+    end
+  end
 
-    {number, sum - number}
+  defp read_numbers(path) do
+    path
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> MapSet.new()
   end
 end
