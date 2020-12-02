5be281344d5d5830528d0bc57f04a46d9052a3ae
Hannes Nevalainen <hannes.nevalainen@me.com>
2020-12-02 18:34:12 +0100

    day 2 part 2

--- lib/02/day02.ex
+++ lib/02/day02.ex
@@ -1,13 +1,24 @@
 defmodule Aoc2020.Day02 do
   def part1() do
-    File.read!("lib/02/input.txt")
-    |> String.split("\n", trime: true)
-    |> Enum.map(fn line ->
-      [policy, password] = String.split(line, ":")
-      {parse_policy(policy), password}
-    end)
+    input_stream("lib/02/input.txt")
     |> Enum.count(fn {policy, password} -> valid_password?(password, policy) end)
     |> IO.inspect(label: "part1")
+  end
+
+  def part2() do
+    input_stream("lib/02/input.txt")
+    |> Enum.count(fn {policy, password} ->
+      valid_password2?(password, policy)
+    end)
+    |> IO.inspect(label: "part2")
+  end
+
+  defp input_stream(path) do
+    File.stream!(path)
+    |> Stream.map(fn line ->
+      [policy, password] = String.split(line, ":")
+      {parse_policy(policy), String.trim(password)}
+    end)
   end
 
   def parse_policy(policy) do
@@ -18,11 +29,16 @@ defmodule Aoc2020.Day02 do
   end
 
   def valid_password?(password, {min, max, char}) do
-    found_chars =
-      password
-      |> String.codepoints()
-      |> Enum.count(fn x -> x === char end)
-
-    found_chars >= min && found_chars <= max
+    password
+    |> String.graphemes()
+    |> Enum.count(fn x -> x === char end)
+    |> Kernel.in(min..max)
   end
+
+  @spec valid_password2?(binary, {integer, integer, any}) :: boolean
+  def valid_password2?(password, {a, b, char}) do
+    xor(String.at(password, a - 1) === char, String.at(password, b - 1) === char)
+  end
+
+  defp xor(a, b) when is_boolean(a) and is_boolean(b), do: a != b
 end
--- test/day02_test.exs
+++ test/day02_test.exs
@@ -1,5 +1,5 @@
 defmodule Day02Test do
-  use ExUnit.Case
+  use ExUnit.Case, aync: true
   alias Aoc2020.Day02
 
   test "parse_policy" do
@@ -11,5 +11,11 @@ defmodule Day02Test do
     assert true === Day02.valid_password?("abcde", {1, 3, "a"})
     assert false === Day02.valid_password?("cdefg", {1, 3, "b"})
     assert true === Day02.valid_password?("ccccccccc", {2, 9, "c"})
+  end
+
+  test "valid_password2?" do
+    assert true === Day02.valid_password2?("abcde", {1, 3, "a"})
+    assert false === Day02.valid_password2?("cdefg", {1, 3, "b"})
+    assert false === Day02.valid_password2?("ccccccccc", {2, 9, "c"})
   end
 end
