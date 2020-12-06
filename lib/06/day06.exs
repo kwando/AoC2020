c0f1b59ec39b1a3eff02a1d2ddf7c7277717273d
Hannes Nevalainen <hannes.nevalainen@me.com>
2020-12-06 12:23:43 +0100

    day6 part2

--- lib/06/day06.exs
+++ lib/06/day06.exs
@@ -1,22 +1,46 @@
 defmodule Aoc2020.Day06 do
+  use Bitwise
+
   def part1(input) do
     input
-    |> Stream.map(&unique_chars/1)
+    |> Stream.map(&count_uniq/1)
     |> Enum.sum()
   end
 
-  def unique_chars(group) do
-    for member <- group, <<char <- member>>, reduce: MapSet.new() do
-      set ->
-        MapSet.put(set, char)
-    end
-    |> MapSet.size()
+  def part2(input) do
+    input
+    |> Stream.map(&count_common/1)
+    |> Enum.sum()
+  end
+
+  defp count_uniq([member | group]) do
+    count_uniq(group, member)
+  end
+
+  defp count_uniq([], value), do: count_bits(value)
+
+  defp count_uniq([member | group], value) do
+    count_uniq(group, member ||| value)
+  end
+
+  defp count_common([member | rest]), do: count_common(rest, member)
+
+  defp count_common([], value), do: count_bits(value)
+
+  defp count_common([member | group], value) do
+    count_common(group, member &&& value)
+  end
+
+  defp count_bits(number) do
+    number
+    |> Integer.digits(2)
+    |> Enum.sum()
   end
 
   def input_stream(path) do
     chunk_fun = fn
       "", parts -> {:cont, parts, []}
-      part, parts -> {:cont, [part | parts]}
+      part, parts -> {:cont, [encode(part) | parts]}
     end
 
     after_fun = fn
@@ -27,9 +51,15 @@ defmodule Aoc2020.Day06 do
     |> Stream.map(&String.trim/1)
     |> Stream.chunk_while([], chunk_fun, after_fun)
   end
+
+  def encode(""), do: 0
+  def encode(<<char, rest::binary()>>), do: 0b1 <<< (char - ?a) ||| encode(rest)
 end
 
-input = Aoc2020.Day06.input_stream("input.txt")
+input = Aoc2020.Day06.input_stream("example.txt")
 
 Aoc2020.Day06.part1(input)
 |> IO.inspect(label: "part1")
+
+Aoc2020.Day06.part2(input)
+|> IO.inspect(label: "part2")
